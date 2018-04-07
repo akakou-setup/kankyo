97138024be51a2a7ca43fb0dfccb64a7d5d0ff18
akakou <s16003@tokyo.kosen-ac.jp>
2018-04-08 01:27:21 +0900

    ユーザ追加等を記述

---
+++ setup/1.root.sh
@@ -0,0 +1,12 @@
+pacman -S neovim zsh zsh-completion
+chsh -s /bin/zsh
+
+useradd akakou -m -g users -G wheel -s /bin/zsh
+passwd akakou
+visudo
+
+timedatectl set-ntp true
+
+echo "[multilib]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+pacman --sync --refresh yaourt

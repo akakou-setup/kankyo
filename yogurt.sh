dc7455aa8d1c27e9f7a784b79661ddd4be182326
akakou <s16003@tokyo.kosen-ac.jp>
2018-04-07 23:47:42 +0900

    よ〜ぐると♡

---
+++ yogurt.sh
@@ -0,0 +1,11 @@
+yaourt -S neovim zsh zsh-completion
+ xf86-video-intel mesa\
+ xorg-server xorg-server-utils xorg-xinit xterm\
+ otf-ipafont ibus mozc\
+ budgie-desktop tilix\
+ network-manager dconf dconf-editor\
+ nautilus gedit gnome-tweak-tool\
+ pip ipython2 ipython3\
+ gtk-engine-murrine gtk-engines\ 
+ numix-circle-icon-theme-git vimix-gtk-themes-git\
+ google-chrome-stable wireshark-gtk

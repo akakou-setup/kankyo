9ee8c22016f0eb4be5cd6d17d904195f423edfbc
akakou <s16003@tokyo.kosen-ac.jp>
2018-04-08 01:39:51 +0900

    &&を追加

--- setup/1/root.sh
+++ setup/1/root.sh
@@ -1,12 +1,12 @@
-pacman -S neovim zsh zsh-completion
-chsh -s /bin/zsh
+pacman -S neovim zsh zsh-completions&&
+  chsh -s /bin/zsh&&
 
-useradd akakou -m -g users -G wheel -s /bin/zsh
-passwd akakou
-visudo
+  useradd akakou -m -g users -G wheel -s /bin/zsh&&
+  passwd akakou&&
+  visudo&&
 
-timedatectl set-ntp true
+  timedatectl set-ntp true&&
 
-echo "[multilib]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
-pacman --sync --refresh yaourt
+  echo "[multilib]" >> /etc/pacman.conf&&
+  echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf&&
+  pacman --sync --refresh yaourt
